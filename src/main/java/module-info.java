2e8b458c08490dc724899c58ec0b005b09e38cf1
PPavell234 <varvardidl@gmail.com>
2025-09-16 20:38:34 +1000

    Файл запускается PDF

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.kmpjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.apache.pdfbox;
-
-
-    opens com.example.kmpjavafx to javafx.fxml;
-    exports com.example.kmpjavafx;
-}
