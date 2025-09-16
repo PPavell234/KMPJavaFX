2a7285915115d08c22651526096f357cf0e33a4f
PPavell234 <varvardidl@gmail.com>
2025-09-16 18:47:00 +1000

    Провекра

--- src/main/java/com/example/kmpjavafx/Main.java
+++ src/main/java/com/example/kmpjavafx/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sample.fxml"));
-        primaryStage.setTitle("КМП поиск (JavaFX)");
+        primaryStage.setTitle("КМП поиск (JavaFX)!!!");
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         primaryStage.setScene(scene);
         primaryStage.show();
