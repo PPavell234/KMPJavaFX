b88c570e53214ce04272445999b0bdc17c8c1b70
PPavell234 <varvardidl@gmail.com>
2025-09-19 16:51:44 +1000

    Добавлен переход на страницу

--- src/main/java/com/example/kmpjavafx/PdfPageController.java
+++ src/main/java/com/example/kmpjavafx/PdfPageController.java
@@ -1,12 +1,19 @@
 package com.example.kmpjavafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
+import java.io.IOException;
 
 public class PdfPageController {
 
@@ -31,6 +38,21 @@ public class PdfPageController {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @FXML
+    private void openHelloView(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/example/kmpjavafx/pdfPage.fxml")
+            );
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
