0d150dc522f8d84d534db7ff60b6e59f9a2bb62f
PPavell234 <varvardidl@gmail.com>
2025-09-19 18:44:08 +1000

    Добавления буффера

--- src/main/java/com/example/kmpjavafx/InfoController.java
+++ src/main/java/com/example/kmpjavafx/InfoController.java
@@ -1,4 +1,56 @@
 package com.example.kmpjavafx;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class InfoController {
+    @FXML
+    private void BackIsInfoPage(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/example/kmpjavafx/startPage.fxml")
+            );
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @FXML
+    private TextFlow textFlow; // свяжи с fx:id в FXML
+
+    @FXML
+    private void CopyTextBuffer(MouseEvent event) {
+        // собрать текст из TextFlow
+        StringBuilder sb = new StringBuilder();
+        for (Node node : textFlow.getChildren()) {
+            if (node instanceof Text) {
+                sb.append(((Text) node).getText());
+            }
+        }
+
+        // положить в буфер обмена
+        ClipboardContent content = new ClipboardContent();
+        content.putString(sb.toString());
+        Clipboard.getSystemClipboard().setContent(content);
+
+        System.out.println("Текст скопирован в буфер!");
+    }
+
 }
